ec0c1e68b8e4fb7c581477f6b7d46165a5863c5b
sushilburagute <codetastic101@gmail.com>
2022-03-01 10:52:00 +0530

    Add: Routes for fetching a dish

--- src/controllers/DishControllers.ts
+++ src/controllers/DishControllers.ts
@@ -1,5 +1,46 @@
 import { RequestHandler, Response } from "express";
+import { Dish } from "../models/Dish";
 
-export const dishList: RequestHandler = (req, res) => {
-  res.json({ msg: "All dishes" });
+export const dishList: RequestHandler = async (req, res) => {
+  try {
+    const allDishes = await Dish.find({});
+
+    if (!allDishes) {
+      return res.status(404).json({
+        msg: "We couldn't retrive the Dishes at the moment.",
+      });
+    }
+
+    res.status(200).json({
+      msg: "Dishes Fetched!",
+      allDishes,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      msg: error,
+    });
+  }
+};
+
+export const findDish: RequestHandler = async (req, res) => {
+  try {
+    const dishItem = await Dish.find({ _id: req.params.id });
+    if (!dishItem) {
+      return res.status(404).json({
+        msg: "Sorry! That dish doesn't exist on our database.",
+      });
+    }
+
+    res.status(200).json({
+      msg: "Dish Fetched!",
+      dishItem,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      msg: "Sorry! That dish doesn't exist on our database.",
+      error: error,
+    });
+  }
 };
--- src/router/FoodRoutes.ts
+++ src/router/FoodRoutes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { dishList } from "../controllers/DishControllers";
+import { dishList, findDish } from "../controllers/DishControllers";
 
 const router = Router();
 
 router.route("/").get(dishList);
-router.route("/:id").get();
+router.route("/:id").get(findDish);
 export = router;
