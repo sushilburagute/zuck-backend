7ee3de5304d45ed008f31d86d05da27edc13d5eb
sushilburagute <codetastic101@gmail.com>
2022-03-25 16:32:13 +0530

    Update corsOptions.ts

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
   // origin: ["http://localhost:3000", "http://localhost:5000", 'https://zuck-food.vercel.app/'],
-  origin: ["https://zuck-food.vercel.app/", 'https://zuck-backend.up.railway.app/'],
+  origin: ["https://zuck-food.vercel.app", "https://zuck-backend.up.railway.app"],
   optionsSuccessStatus: 200,
   credentials: true,
   exposedHeaders: ["set-cookie"],
