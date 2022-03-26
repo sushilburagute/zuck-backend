25b61505d9b53b8dd89b3c5d1be00d3c4ab387d3
sushilburagute <codetastic101@gmail.com>
2022-03-26 13:05:02 +0530

    Add: dev branch's cors settings

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,8 +1,8 @@
 import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
-  // origin: ["http://localhost:3000", "http://localhost:5000", 'https://zuck-food.vercel.app/'],
-  origin: ["https://zuck-food.vercel.app", "https://zuck-backend.up.railway.app"],
+  // origin: ["http://localhost:3000", "http://localhost:5000"],
+  origin: ["https://zuck-food.vercel.app", "https://zuck-backend.up.railway.app", 'https://zuck-frontend-git-development-sushilburagute.vercel.app'],
   optionsSuccessStatus: 200,
   credentials: true,
   exposedHeaders: ["set-cookie"],
